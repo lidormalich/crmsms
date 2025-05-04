62bef1b186af28399161bd15441b73c283554024
Lidor Repo <lidormalich@gmail.com>
2025-05-04 20:47:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 3900;
 const bodyParser = require('body-parser')
 
 // All Route
-
 const routes = require('./routes/index')
 const login = require('./routes/login')
 const event = require('./routes/event')
@@ -24,7 +23,7 @@ process.on("uncaughtException", function (err) {
   console.log("Node NOT Exiting...");
 });
 
-app.use(cors()) //Open To all
+app.use(cors()) // Open To all
 require('dotenv').config();
 mongoose.set('strictQuery', false);
 
@@ -32,9 +31,28 @@ mongoose.Promise = global.Promise
 mongoose.connect(process.env.DB, { useNewUrlParser: true })
     .then(() => { console.log('conected to Data base') })
     .catch((err) => console.log(err))
-app.use(bodyParser.json())
 
-// Use all Route
+// Middleware setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+// Define static options
+const reactOptions = {
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set("Cache-Control", "no-cache");
+  },
+  extensions: ["html", "htm"],
+};
+
+// Static files - Frontend React app
+const buildPath = path.join(__dirname, "public/crmsms/build");
+app.use(express.static(buildPath, reactOptions));
+
+// API Routes
 app.use('/api/login', login);
 app.use('/api/register', register);
 app.use('/api/event', event);
@@ -43,60 +61,19 @@ app.use('/api/sendsms', sendsms);
 app.use('/api/sentences', sentences);
 app.use('/api/excel', excel);
 
+// Error handling middleware
 app.use((err, req, res, next) => {
     console.log(err);
     console.log(req);
     next();
-})
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Catch-all route for SPA (React) - must be after API routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'));
+});
 
-app.use(bodyParser.json());
+// Start the server
 app.listen(port, () => {
     console.log('server conected on port: ' + port);
-})
-
-// static page
-// Root
-const reactOptions = {
-  redirect: false,
-  setHeaders: function (res, path, stat) {
-    res.set("Cache-Control", "no-cache");
-  },
-  extensions: ["html", "htm"],
-};
-const root = require("path").join(__dirname, "public");
-app.use(
-  "/",
-  express.static(
-    require("path").join(__dirname, "public/crmsms/build"),
-    reactOptions
-  )
-);
-
-app.use(
-  "/",
-  express.static(
-    require("path").join(__dirname, "public/crmsms/build"),
-    reactOptions
-  )
-);
-app.use("/app/:route", (req, res, next) => {
-  // console.log(req.params.route);
-  // if (req.params.route === "api") return next();
-  res.sendFile("index.html", { root: root + "/crmsms/build" });
-});
-
-app.use("/", express.static(root, reactOptions));
-// Add slash to the end of the url path
-app.use("/", (req, res, next) => {
-  req.path.slice(-1) == "/" ? next() : res.redirect(req.path + "/");
-});
-
-app.use("/:route", (req, res, next) => {
-  res.sendFile("index.html", { root: root + "/crmsms/build" });
 });
