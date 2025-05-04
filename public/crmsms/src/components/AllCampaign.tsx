d82faf80bdfeb5349d9063b5b319bdf249314632
Lidor Repo <lidormalich@gmail.com>
2025-05-04 21:15:12 +0300

    .

--- public/crmsms/src/components/AllCampaign.tsx
+++ public/crmsms/src/components/AllCampaign.tsx
@@ -43,8 +43,7 @@ const AllCampaign: FunctionComponent<AllCampaignProps> = () => {
                         {allEvent.map((eventItem: EventInterface) => <tr key={counter}>
                             <td>{counter++}</td>
                             <td>{eventItem.campaignName}</td>
-                            {/* <td><Link to={`/group/${eventItem._id}`}><img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/user-management-3497686-2927497.png" height={30} alt="" /></Link></td> */}
-                            <td><Link to={`/campaign/${eventItem._id}`}><img src="https://cdn-icons-png.flaticon.com/512/32/32355.png" height={30} alt="" /></Link></td>
+                                                        <td><Link to={`/campaign/${eventItem._id}`}><img src="https://cdn-icons-png.flaticon.com/512/32/32355.png" height={30} alt="" /></Link></td>
                             <td onClick={() => {
                                 setEventID(eventItem._id as string)
                                 setOpendeleteModal(true);
